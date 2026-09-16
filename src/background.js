833fc5d19ab312d539903c7604c75778f4d1acd6
lubezhang <lubezhang@gmail.com>
2026-09-16 14:20:19 +0800

    Redesign popup for product and order workflows

--- src/background.js
+++ src/background.js
@@ -1,29 +1,10 @@
-const SESSION_ORDER_PASSWORD_KEY = "sessionOrderPassword";
-const SESSION_ORDER_TICKET_KEY = "sessionOrderVerificationTicket";
+const session = chrome.storage.session;
 
 chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
-  if (message.type === "get-session-order-password") {
-    chrome.storage.session.get({ [SESSION_ORDER_PASSWORD_KEY]: "" })
-      .then((values) => sendResponse({ password: values[SESSION_ORDER_PASSWORD_KEY] || "" }))
-      .catch(() => sendResponse({ password: "" }));
-    return true;
-  }
-  if (message.type === "get-session-order-ticket") {
-    chrome.storage.session.get({ [SESSION_ORDER_TICKET_KEY]: "" })
-      .then((values) => sendResponse({ ticket: values[SESSION_ORDER_TICKET_KEY] || "" }))
-      .catch(() => sendResponse({ ticket: "" }));
-    return true;
-  }
-  if (message.type === "set-session-order-ticket") {
-    chrome.storage.session.set({ [SESSION_ORDER_TICKET_KEY]: message.ticket || "" })
-      .then(() => sendResponse({ saved: true }))
-      .catch(() => sendResponse({ saved: false }));
-    return true;
-  }
-  if (message.type === "clear-session-order-ticket") {
-    chrome.storage.session.remove(SESSION_ORDER_TICKET_KEY)
-      .then(() => sendResponse({ cleared: true }))
-      .catch(() => sendResponse({ cleared: false }));
-    return true;
-  }
+  if (!message?.type?.startsWith("session-")) return;
+  const key = message.type === "session-password" ? "sessionOrderPassword" : "sessionOrderVerificationTicket";
+  if (message.action === "get") session.get(key).then((value) => sendResponse({ value: value[key] || "" }));
+  if (message.action === "set") session.set({ [key]: message.value || "" }).then(() => sendResponse({ saved: true }));
+  if (message.action === "clear") session.remove(key).then(() => sendResponse({ cleared: true }));
+  return true;
 });
